1aef125fa73f91601c75be40f21d719dbce93000
Alexander Mulyar <mulyar.alexander0503@yandex.ru>
2024-03-20 19:35:25 +0500

    Creating the PasswordGenerator class

---
+++ AMPasswordGenerator/PasswordGenerator.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMPasswordGenerator
+{
+    internal class PasswordGenerator
+    {
+        Random random = new Random();
+
+        public string GetCharUp()
+        {
+            string[] charsUp = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+            return charsUp[random.Next(26)];
+        }
+
+        public string GetCharLow()
+        {
+            string[] charsLow = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "k", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+            return charsLow[random.Next(26)];
+        }
+
+        public string GetDigits()
+        {
+            string[] digits = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+            return digits[random.Next(10)];
+        }
+
+        public string GetSpChar()
+        {
+            string[] spChars = { "*", "#", "+", "-", "$", "&", "<", ">", "(", ")" };
+            return spChars[random.Next(10)];
+        }
+
+        public string GetPasswordString(bool charUp, bool charLow, bool digit, bool spChar, int length)
+        {
+            string passwordString = "";
+            int i = 0;
+            int iChar = 0;
+
+            while (i < length)
+            {
+                iChar = random.Next(4);
+
+                if (charUp == true & iChar == 0)
+                {
+                    passwordString += GetCharUp();
+                }
+
+                if (charLow == true & iChar == 1)
+                {
+                    passwordString += GetCharLow();
+                }
+
+                if (digit == true & iChar == 2)
+                {
+                    passwordString += GetDigits();
+                }
+
+                if (spChar == true & iChar == 3)
+                {
+                    passwordString += GetSpChar();
+                }
+
+                i = passwordString.Length;
+            }
+
+            return passwordString;
+        }
+    }
+}
